05e75543f2ef12e98e0162b3578af77b3bf582c2
kuwabara kazuma <ponkotsu.hiyorin@gmail.com>
2019-02-02 03:26:59 +0900

    [remove] dotween modules

--- Scripts/AudioSourceExtensions.cs
+++ Scripts/AudioSourceExtensions.cs
@@ -1,11 +1,8 @@
 ﻿using UnityEngine;
-#if EXTENSIONS_DOTWEEN
-using DG.Tweening;
-#endif
 
 namespace UnityExtensions
 {
-    public static partial class AudioSourceExtensions
+    public static class AudioSourceExtensions
     {
         public static bool Play(this AudioSource self, AudioClip clip, float volume)
         {
@@ -17,53 +14,5 @@ namespace UnityExtensions
             self.Play();
             return true;
         }
-
-#if EXTENSIONS_DOTWEEN
-        public static void FadeIn(this AudioSource self, float volume, float duration)
-        {
-            if (volume < 0.0f) volume = 0.0f;
-            else if (volume > 1.0f) volume = 1.0f;
-            DOTween.To(() => self.volume, x => self.volume = x, volume, duration).SetTarget(self);
-        }
-    
-        public static IEnumerator FadeInAsCoroutine(this AudioSource self, float volume, float duration)
-        {
-            self.FadeIn(volume, duration);
-            yield return new WaitForSeconds(duration);
-        }
-    
-        public static void FadeOut(this AudioSource self, float duration)
-        {
-            DOTween.To(() => self.volume, x => self.volume = x, 0.0f, duration).SetTarget(self);
-        }
-
-        public static IEnumerator FadeOutAsCoroutine(this AudioSource self, float duration)
-        {
-            self.FadeOut(duration);
-            yield return new WaitForSeconds(duration);
-        }
-    
-        public static bool PlayWithFadeIn(this AudioSource self, AudioClip clip, float volume, float duration)
-        {
-            if (clip == null || volume <= 0.0f)
-                return false;
-
-            self.clip = clip;
-            self.volume = 0.0f;
-            self.Play();
-            self.FadeIn(volume, duration);
-            return true;
-        }
-#endif
-
-#if EXTENSIONS_DOTWEEN && EXTENSIONS_UNIRX
-        public static IObservable<Unit> PlayWithFadeInAsObservable(this AudioSource self, AudioClip clip, float volume, float duration)
-        {
-            if (self.PlayWithFadeIn(clip, volume, duration))
-                return Observable.Timer(TimeSpan.FromSeconds(clip.length)).AsUnitObservable();
-            else
-                return Observable.Throw<Unit>(new ArgumentException());
-        }
-#endif
     }
 }
--- Scripts/FloatExtensions.cs
+++ Scripts/FloatExtensions.cs
@@ -2,11 +2,11 @@
 
 namespace UnityExtensions
 {
-	public static class FloatExtensions
-	{
-		public static bool ApproxEquals(this float f, float f2)
-		{
-			return Mathf.Approximately(f, f2);
-		}
-	}
+    public static class FloatExtensions
+    {
+        public static bool ApproxEquals(this float f, float f2)
+        {
+            return Mathf.Approximately(f, f2);
+        }
+    }
 }
